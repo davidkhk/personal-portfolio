0117f014551ab7241c2d084f3be4d50da0d1c1f8
David <davidkhk@gmail.com>
2022-01-27 12:35:10 -0300

    Change contactForm to deploy on Netlify

---
+++ src/components/contactForm/contactForm.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+import './styles.css';
+
+const StyledButton = styled.button`
+  color: var(--clr-dark-blue);
+  background-color: var(--clr-blue);
+  border: 1px solid var(--clr-secondary1);
+  width: 15%;
+  height: 30px;
+  border-radius: var(--border-radius);
+  margin: 2% 0 10% 0;
+  text-align: center;
+  cursor: pointer;
+  font-family: 'Futura', 'Mr Eaves Sans', 'Futura PT Book';
+  transition: 0.3s;
+  
+  :hover {
+    color: var(--clr-blue);
+    background: var(--clr-dark-blue);
+    border: 1px var(--clr-blue) solid;
+  }
+`
+
+const ContactForm = () => {
+  return (
+    <form className="form" name='contact' method='post'>
+      <input className="input" type="hidden" name="form-name" value="contact" />
+      <>
+        <label htmlFor='name'></label>
+        <input className="input" type='text' id='name' name='name' placeholder="What's yout name?" required />
+      </>
+      <>
+        <label htmlFor='email'></label>
+        <input className="input" type='email' id='email' name='email' placeholder="What's your email?" required />
+      </>
+      <>
+        <label htmlFor='message'></label>
+        <textarea className="input" id='message' name='message' placeholder='Type your message!' rows='8' required />
+      </>
+      <StyledButton type='submit'>Send</StyledButton>
+    </form>
+  );
+};
+
+export default ContactForm;
--- src/components/sections/contact.jsx
+++ src/components/sections/contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import ContactForm from '../contactForm';
+import ContactForm from '../contactForm/contactForm';
 
 const Wrapper = styled.section`
   background-image: linear-gradient(to bottom, #f6f6f6, #f6f6f6, #f6f6f6, #f6f6f6, #f6f6f6, #f2f1f5, #ededf3, #e7e9f2, #d9e0ef, #c8d8ec, #b6d1e8, #a1cae2);
