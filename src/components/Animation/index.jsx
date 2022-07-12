f6d49411be325b2e9587e3b0d00141ccaa5eac99
David Kang <davidkhk@gmail.com>
2022-07-12 15:59:50 -0300

    add animation

---
+++ src/components/Animation/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { motion } from 'framer-motion';
+
+/**
+ * @description A component created for simpler use of framer motion's fade-in animation. It checks if the component is in the viewport and if so it initiates the fade-in animation.
+ *
+ * @param {number} timeout Duration for the fade-in in milliseconds(ms).
+ *
+ * @return A SectionAnimation react component.
+ */
+export function SectionAnimation({ ...props }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ duration: props.timeout / 1000 }}
+      style={{ width: props.w || 'auto' }}
+    >
+      {props.children}
+    </motion.div>
+  );
+}
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Wrapper, StyledDiv } from './index-style';
 import { FaAngleDown } from 'react-icons/fa';
+import { SectionAnimation } from '../Animation';
 
-const Home = () => {
+const Home = ({ ...props }) => {
   return (
     <Wrapper id='home'>
-      <StyledDiv>
-        <h3>Hi there! My name is</h3>
-        <div>
-          <h1>David Kang</h1>
-        </div>
-        <p>software developer</p>
-      </StyledDiv>
+      <SectionAnimation timeout={2000}>
+        <StyledDiv>
+          <h3>Hi there! My name is</h3>
+          <div>
+            <h1>David Kang</h1>
+          </div>
+          <p>software developer</p>
+        </StyledDiv>
+      </SectionAnimation>
       <a href='#work'>
         <FaAngleDown />
       </a>
