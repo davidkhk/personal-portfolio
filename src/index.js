b13ce7aae4d72cb905c60b995840340f1b118a20
David Kang <davidkhk@gmail.com>
2022-07-13 16:43:54 -0300

    adapt index.js to new dep versions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import GlobalStyles from './styles/globalStyle';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
